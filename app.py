421601f3974c2c61d85096d0b345935c4497aab3
Anshul Singh <128998310+yodamynameis@users.noreply.github.com>
2025-04-09 01:43:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -212,7 +212,7 @@ def main():
         # Preprocess the resume text
         cleaned_resume = preprocess_text(resume_text)
         vectorizer = TfidfVectorizer()
-        vectorizer.fit(df['Resume'])
+        vectorizer.fit(df['cleaned_resume'])
 
         # Transform the resume text using the fitted vectorizer
         resume_vector = vectorizer.transform([cleaned_resume])
